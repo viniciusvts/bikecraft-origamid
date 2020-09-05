462745339ce5f1de76b81e6f093ebb750c9028af
Vinicius de Santana <vinicius.vts@gmail.com>
2020-09-05 18:29:23 -0300

    feat: slide

---
+++ js/script.js
@@ -0,0 +1,11 @@
+if (window.SimpleSlide) {
+    new SimpleSlide({
+        slide: 'quote',
+        time: 5000
+    })
+    new SimpleSlide({
+        slide: 'port',
+        time: 5000,
+        nav: true
+    })
+}
